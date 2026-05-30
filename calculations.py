f4df391ec370e3402796d0c60ea9374bf16e7454
Brian Tenholder <peter1090ten@gmail.com>
2026-05-30 17:35:58 -0400

    Saving progress

--- calculations.py
+++ calculations.py
@@ -5,13 +5,13 @@ def calculate_A(width):
     return (1 / (np.pi * 2 * width**2)) ** 0.25
 
 def create_wave_packet(x, t=0):
-    return calculate_A(INITIAL_WIDTH) * ((-1 * (x-INITIAL_POSITION)**2) / (4 * INITIAL_WIDTH**2)) * np.exp(np.sqrt(-1) * K_O * x)
+    return calculate_A(INITIAL_WIDTH) * ((-1 * (x-INITIAL_POSITION)**2) / (4 * INITIAL_WIDTH**2)) * np.exp(1j * K_O * x)
 
 def calculate_evolution(x, t, width, group_velocity):
     A = calculate_A(width)
-    return A * (1 / (np.sqrt(1+np.sqrt(-1) * (HBAR * t / (2 * MASS * INITIAL_WIDTH**2))))) * np.exp(-1 * (x -INITIAL_POSITION - group_velocity * t)**2 / (4 * width**2) * ( 1 - np.sqrt(-1) * (HBAR * t / (2 * MASS * INITIAL_WIDTH**2))) + np.sqrt(-1) *K_O * x - np.sqrt(-1) * (HBAR * K_O**2 * t / (2 * MASS))) 
+    return A * (1 / (np.sqrt(1+1j * (HBAR * t / (2 * MASS * INITIAL_WIDTH**2))))) * np.exp(-1 * (x -INITIAL_POSITION - group_velocity * t)**2 / (4 * width**2) * ( 1 - 1j * (HBAR * t / (2 * MASS * INITIAL_WIDTH**2))) + 1j *K_O * x - 1j * (HBAR * K_O**2 * t / (2 * MASS))) 
 
 def nomrmalize_wave_packet(wave_packet, x):
-    norm = np.trapz(np.abs(wave_packet)**2, x)
+    norm = np.trapezoid(np.abs(wave_packet)**2, x)
     return wave_packet / np.sqrt(norm)
 
--- main.py
+++ main.py
@@ -26,23 +26,25 @@ print("Momentumn Grid Shape: ", PX.shape)
 
 inital_packet = create_wave_packet(X) # maybe just X
 normalized_packet = nomrmalize_wave_packet(inital_packet, x_axis)
+for i in range(10):
+    # first half kick 
+    # mulitpoly position wave funciton by first half of potential energy operator V(x) 
+    psi_new = normalized_packet * np.exp(-1 *1j * V * dt / (2 * HBAR))
 
-# first half kick 
-# mulitpoly position wave funciton by first half of potential energy operator V(x) 
-psi_new = normalized_packet * np.exp(-1 * np.sqrt(-1) * V * dt / (2 * HBAR))
-
-# transform wave function from position space into momentum space with fft
-phi_new = np.fft.fftn(psi_new)
+    # transform wave function from position space into momentum space with fft
+    phi_new = np.fft.fftn(psi_new)
 
 
-# kinetic drift
-# multiply kinetic energy operator in momentum space 
-phi_new_drifted = phi_new * np.exp(-1 * np.sqrt(-1) * HBAR * (PX**2 + PY**2 + PZ**2) * dt / (2 * MASS)) # k^2 = px^2 + py^2 + pz^2
+    # kinetic drift
+    # multiply kinetic energy operator in momentum space 
+    phi_new_drifted = phi_new * np.exp(-1 *1j * HBAR * (PX**2 + PY**2 + PZ**2) * dt / (2 * MASS)) # k^2 = px^2 + py^2 + pz^2
 
-# reutn to position space with inverse fft
+    # reutn to position space with inverse fft
 
-psi_new_drifted = np.fft.ifftn(phi_new_drifted)
+    psi_new_drifted = np.fft.ifftn(phi_new_drifted)
 
-# second half kick 
-# multoply by second half of potential energy operator V(x) to get final wave function at time t+dt
-psi_final = psi_new_drifted * np.exp(-1 * np.sqrt(-1) * V * dt / (2 * HBAR))
+    # second half kick 
+    # multoply by second half of potential energy operator V(x) to get final wave function at time t+dt
+    psi_final = psi_new_drifted * np.exp(-1 *1j * V * dt / (2 * HBAR))
+
+    print(np.trapezoid(np.abs(psi_final)**2, x_axis)) # should be 1, check normalization
